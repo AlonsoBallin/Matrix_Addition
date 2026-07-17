40251af65972588b94eaea3f11ead3d61e1290eb
AlonsoReyesIDK <a.dominguez.11r@gmail.com>
2026-07-16 22:49:54 -0600

    Completed 2 methods
    Completed the method to read a matrix and to print out the matrix.

--- Main.java
+++ Main.java
@@ -21,19 +21,38 @@ public class Main
 			System.exit(1);
 		}
 		
-		// TODO Fill in your code here
+		int rows = inFile.nextInt();
+		int columns = inFile.nextInt(); 
+
+		int[][] A = readInMatrix(rows, columns, inFile); 
+		int[][] B = readInMatrix(rows, columns, inFile);  
+		int[][] C = new int[rows][columns]; 
+
+		print2dArray(A);   
+		
 	}	
 
 	public static int[][] readInMatrix(int rows, int columns, Scanner inFile)
 	{
 		int[][] toReturn = new int[rows][columns];
-		// TODO Fill in your code here
+		
+		for(int i = 0; i < rows; i++){ 
+			for(int j = 0; j < columns; j++) { 
+				toReturn[i][j] = inFile.nextInt();
+			}
+		} 
+
 		return toReturn;
 	}
 
 	public static void print2dArray(int[][] matrix)
 	{
-		
+		for(int i = 0; i < matrix.length; i++){  
+			for(int j = 0; j < matrix[i].length; j++){ 
+				System.out.print(matrix[i][j] + " ");    
+			}   
+			System.out.println(); 
+		}
 	}
 
 }
